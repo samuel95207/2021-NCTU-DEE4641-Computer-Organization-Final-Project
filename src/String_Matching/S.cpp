302c869f8aff3560c3a10b5297469e530064b609
Samuel Huang <lspss95207@gmail.com>
2021-05-02 15:59:56 +0800

    Improve search

--- src/String_Matching/S.cpp
+++ src/String_Matching/S.cpp
@@ -22,16 +22,18 @@ void search(char *pat, char *txt, int *suffArr, int n)
         const char * cmpStr = txt+suffArr[mid];
         int res = strncmp(pat, cmpStr, strlen(cmpStr));
         if (res < 0) r = mid;
+        else if (res == 0) break;
         else l = mid + 1;
     }
     int top = l;
 
     // Find the bottom index
+    const size_t pat_size = strlen(pat);
     l = 0, r = n-1;
     while (l != r)
     {
         int mid = (l + r + 1) / 2;
-        int res = strncmp(pat, txt+suffArr[mid], strlen(pat));
+        int res = strncmp(pat, txt+suffArr[mid], pat_size);
         if (res == 0 || res > 0) l = mid;
         else r = mid - 1;
     }
@@ -40,6 +42,7 @@ void search(char *pat, char *txt, int *suffArr, int n)
     for(int i = top; i <= down; i++){
         cout << "pattern matched at pos " << suffArr[i] << "\n";
     }
+    // cout<<"hi"<<endl;
 }
 
 // Driver program to test above functions
