db4e9a37167351f08f015e8b4f105d90bc5fa9ac
Samuel Huang <lspss95207@gmail.com>
2021-05-31 16:05:27 +0800

    Link FM_index into docker

--- docker_run.ps1
+++ docker_run.ps1
@@ -5,4 +5,5 @@ docker run `
     --name repo_gem5 `
     -it `
     --volume $path\src\String_Matching:/usr/local/src/gem5_profile/String_Matching `
+    --volume $path\src\FM_index:/usr/local/src/gem5_profile/FM_index `
     kyens0612/gem5_profile:latest
